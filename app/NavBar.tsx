ed5b4dc622439cf852f720a8db20fa0b79ca066c
Tafadzwa Musiyiwa <tnmusiyiwa@gmail.com>
2024-09-12 10:50:18 +0200

    style active link in the navBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,11 +1,17 @@
+"use client";
+
 import Link from "next/link";
+import {usePathname} from "next/navigation";
 import React from "react";
 import {AiFillBug} from "react-icons/ai";
+import classNames from "classnames";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
     {label: "Dashboard", href: "/"},
-    {label: "Issues", href: "/"},
+    {label: "Issues", href: "/issues"},
   ];
   return (
     <nav className="flex space-x-6 border-b mb-5 h-14 items-center px-5">
@@ -17,7 +23,11 @@ const NavBar = () => {
           <Link
             key={link.href}
             href={link.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={classNames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
           >
             {link.label}
           </Link>
---
+++ app/issues/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react'
+
+const IssuesPage = () => {
+  return (
+    <div>IssuesPage</div>
+  )
+}
+
+export default IssuesPage

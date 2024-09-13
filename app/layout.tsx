bd3529ee9920c4f31d9eefd2ed2be7cb2a60e0fe
Tafadzwa Musiyiwa <tnmusiyiwa@gmail.com>
2024-09-13 09:32:53 +0200

    Set up prisma

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="retro">
       <body className={inter.className}>
         <NavBar />
         <main>{children}</main>

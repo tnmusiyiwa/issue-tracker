fe2238d1de7bdc8de68add478ed9b2e6871b3547
Tafadzwa Musiyiwa <tnmusiyiwa@gmail.com>
2024-09-14 11:43:42 +0200

    building the new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import {Button} from "@radix-ui/themes";
+import Link from "next/link";
 import React from "react";
 
 const IssuesPage = () => {
   return (
     <div>
-      <Button>New Issue</Button>
+      <Button>
+        <Link href="/issues/new/">New Issue</Link>
+      </Button>
     </div>
   );
 };
